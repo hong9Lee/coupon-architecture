23546481879440e9680db49e398ed6ba7194eae5
hong9Lee <hongtemp9mac@gmail.com>
2024-05-15 18:52:39 +0900

    chore: domain module 추가

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,9 @@
 rootProject.name = "coupon"
 
 include("coupon-adapter:coupon-api")
+
 include("coupon-application:coupon-core")
+include("coupon-application:coupon-domain")
+
+
 include("coupon-infra:coupon-persistence")
