42251e7b0b2bd8a2d96657a986568a551721ca28
hong9Lee <hongtemp9mac@gmail.com>
2024-05-15 18:49:49 +0900

    init

---
+++ coupon-adapter/coupon-api/src/main/kotlin/com/hg/api/CouponApiApplication.kt
@@ -0,0 +1,11 @@
+package com.hg.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CouponApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<CouponApiApplication>(*args)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,5 @@
+rootProject.name = "coupon"
+
+include("coupon-adapter:coupon-api")
+include("coupon-application:coupon-core")
+include("coupon-infra:coupon-persistence")
