6cba6ad8e6ef03f3157228eef6c725f67f5b20c5
hong9Lee <hongtemp9mac@gmail.com>
2024-07-28 15:14:50 +0900

    chore: redis 모듈 추가

--- coupon-adapter/coupon-consumer/src/main/kotlin/com/hg/coupon/CouponConsumerApplication.kt
+++ coupon-adapter/coupon-consumer/src/main/kotlin/com/hg/coupon/CouponConsumerApplication.kt
@@ -1,9 +1,12 @@
 package com.hg.coupon
 
+import com.hg.coupon.infra.redis.config.RedisProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(RedisProperties::class)
 class CouponConsumerApplication
 
 fun main(args: Array<String>) {
